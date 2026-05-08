cf8fd14c17201c78c6f3174cab73cd178b1211c1
revmiller <revmiller@pm.me>
2026-05-09 00:04:10 +0200

    scaffold SE-2 monorepo

---
+++ packages/foundry/script/Deploy.s.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "./DeployHelpers.s.sol";
+
+contract DeployScript is ScaffoldETHDeploy {
+  function run() external {
+    // Add deployments here, e.g.:
+    // DeployHromadaRegistry deployRegistry = new DeployHromadaRegistry();
+    // deployRegistry.run();
+  }
+}
